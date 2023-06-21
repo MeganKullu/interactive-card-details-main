b5cdfd0b841915955007bae8a60018480454abb3
MeganKullu <kullumegan@gmail.com>
2023-06-21 22:09:19 +0300

    mobile design completed

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",
+  "./src/**/*.js",],
+  theme: {
+    extend: {
+      colors :{
+        'white': 'hsl(0, 0%, 100%)',
+        'light-grayish-violet': 'hsl(270, 3%, 87%)',
+        'dark-grayish-violet': 'hsl(279, 6%, 55%)',
+        'very-dark-violet': 'hsl(278, 68%, 11%)',      
+      }
+      
+    },
+  },
+  plugins: [],
+}
+
