12812f7ef9dc3d75a2548022e028f894febf9291
MeganKullu <kullumegan@gmail.com>
2023-07-14 19:00:09 +0300

    added real time validation with event listeners

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 const form = document.getElementById("cardDetails");
 
-form.addEventListener('input', function (e) {
+form.addEventListener(function (e) {
     // prevent the form from submitting
     e.preventDefault();
 
-    if(!validateCreditCardForm()) {
+    if (!validateCreditCardForm()) {
         return false;
     }
 
@@ -16,29 +16,35 @@ form.addEventListener('input', function (e) {
 
 function validateCreditCardForm() {
     // Get the values of the form inputs.
-    
+
     const expDate = document.getElementById("expDate").value;
     const cvv = document.getElementById("cvv").value;
     const monthYear = document.getElementById("mmYy").value;
-    
-    
-    // Check the expiration date format.
-    if (!expDate.match(/^[0-9]{2}\/[0-9]{2}$/)) {
-      document.getElementById("expDateError").innerHTML = "Invalid expiration date";
-      return false;
-    }
 
-    if (!monthYear.match(/^[0-9]{2}\/[0-9]{2}$/)) {
-        document.getElementById("expDateError").innerHTML = "Invalid expiration date";
-        return false;
-    }
-  
+
+    // Check the expiration date format.
+    expDate.addEventListener('input', function () {
+        if (!expDate.match(/^[0-9]{2}\/[0-9]{2}$/)) {
+            document.getElementById("expDateError").innerHTML = "Invalid expiration date";
+            return false;
+        }
+    })
+
+    monthYear.addEventListener('input', function () {
+        if (!monthYear.match(/^[0-9]{2}\/[0-9]{2}$/)) {
+            document.getElementById("expDateError").innerHTML = "Invalid expiration date";
+            return false;
+        }
+    })
+
     // Check the CVV format.
-    if (!cvv.match(/^[0-9]{3}$/)) {
-      document.getElementById("cvvError").innerHTML = "Invalid CVV";
-      return false;
-    }
-  
+    cvv.addEventListener('input', function () {
+        if (!cvv.match(/^[0-9]{3}$/)) {
+            document.getElementById("cvvError").innerHTML = "Invalid CVV";
+            return false;
+        }
+    })
+
     // The form is valid.
     return true;
 }
@@ -49,26 +55,30 @@ function validateCardNumber(cardNumber) {
 
     const cardNumber = document.getElementById("cardNumber").value;
 
-    if (!cardNumber.match(/^[0-9]{16}$/)) {
-      document.getElementById("cardNumberError").innerHTML = "Invalid card number";
-      return false;
-    }
+    cardNumber.addEventListener('input', function () {
+        if (!cardNumber.match(/^[0-9]{16}$/)) {
+            document.getElementById("cardNumberError").innerHTML = "Invalid card number";
+            return false;
+        }
+    })
     // using luhn's algorithm to check further for validity
     let sum = 0;
     for (let i = 0; i < cardNumber.length; i++) {
-      let digit = cardNumber.charAt(i);
-      if (i % 2 == 0) {
-        digit = digit * 2;
-        if (digit > 9) {
-          digit = digit - 9;
+        let digit = cardNumber.charAt(i);
+        if (i % 2 == 0) {
+            digit = digit * 2;
+            if (digit > 9) {
+                digit = digit - 9;
+            }
         }
-      }
-      sum += digit;
+        sum += digit;
     }
+
+    cardNumber.addEventListener('input', function () {
     if (sum % 10 != 0) {
-      document.getElementById("cardNumberError").innerHTML = "Invalid please input Visa or MasterCard ";
-      return false;
-    }
-  
+        document.getElementById("cardNumberError").innerHTML = "Invalid please input Visa or MasterCard ";
+        return false;
+    }})
+
     return true;
 }
